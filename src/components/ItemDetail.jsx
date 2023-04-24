414179cecb8448d328521b1c58395d5a70a77165
Alejandro More <alejandromore23a@hotmail.com>
2023-04-24 17:31:14 -0300

    cambios

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,6 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router';
 import { CartContext } from '../Context/CartContext';
+
+import {doc, getDoc, getDocs, getFirestore,collection}  from 'firebase/firestore'
+
+
 
 const ItemDetail = () => {
 
@@ -8,32 +12,53 @@ const ItemDetail = () => {
 
     const { addToCart, removeFromCart } = useContext(CartContext)
 
-    const products = [
-            {
-            id: 1,
-            name: 'Producto 1',
-            image: 'https://i.ibb.co/CwnBrjN/Puma-Suede.jpg',
-            price: 10,
-            description: 'Descripción del producto 1'
-            },
-            {
-            id: 2,
-            name: 'Producto 2',
-            image: 'https://i.ibb.co/ynBHJ5T/nike-cortez-white.jpg',
-            price: 20,
-            description: 'Descripción del producto 2'
-            },
-            {
-            id: 3,
-            name: 'Producto 3',
-            image: 'https://i.ibb.co/NVwmHHj/Nike-Air-Force-white.jpg',
-            price: 30,
-            description: 'Descripción del producto 3'
-            }
-        ];
+    // const products = [
+    //         {
+    //         id: 1,
+    //         name: 'Producto 1',
+    //         image: 'https://i.ibb.co/CwnBrjN/Puma-Suede.jpg',
+    //         price: 10,
+    //         description: 'Descripción del producto 1'
+    //         },
+    //         {
+    //         id: 2,
+    //         name: 'Producto 2',
+    //         image: 'https://i.ibb.co/ynBHJ5T/nike-cortez-white.jpg',
+    //         price: 20,
+    //         description: 'Descripción del producto 2'
+    //         },
+    //         {
+    //         id: 3,
+    //         name: 'Producto 3',
+    //         image: 'https://i.ibb.co/NVwmHHj/Nike-Air-Force-white.jpg',
+    //         price: 30,
+    //         description: 'Descripción del producto 3'
+    //         }
+    //     ];
         
 
-    const selectItem = products.find(product => product.id == itemId)
+    const [items, setItems] = useState()
+
+
+    useEffect(() => {
+        
+        const db = getFirestore()
+        const ItemsColletion = collection (db, 'items')
+
+        getDocs(ItemsColletion).then((snapshot => {
+            if(snapshot == 0) {
+                console.log("No hay resultados")
+            }
+        setItems(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+        }))
+        
+        
+    }, [])
+    
+
+
+
+    const selectItem = ItemsColletion.find(product => product.id == itemId)
 
 
     return (
